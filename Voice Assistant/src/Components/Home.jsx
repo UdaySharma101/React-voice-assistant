bab018837ae871424239344c6efbd4b2ecc4ffa5
Uday Sharma <udayvashisht101@gmail.com>
2026-03-15 13:05:28 +0530

    First animation added

--- Voice Assistant/src/Components/Home.jsx
+++ Voice Assistant/src/Components/Home.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
+   const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    setShow(true);
+  }, []);
   return (
     <div className="h-screen w-screen bg-linear-to-br from-black via-gray-900 to-black text-white flex flex-col items-center justify-between py-10">
 
       {/* Header */}
       <div className="text-center">
-        <h1 className="text-3xl font-semibold tracking-wide">
+        <h1 className="text-4xl font-semibold tracking-wide animate-bounce">
           Voice Assistant
         </h1>
         <p className="text-gray-400 text-sm mt-2">
@@ -18,11 +23,16 @@ const Home = () => {
       <div className="w-[90%] max-w-xl flex flex-col gap-4">
 
         {/* AI Message */}
-        <div className="bg-gray-800 px-4 py-3 rounded-2xl w-fit max-w-[80%]">
-          <p className="text-sm text-gray-300">AI</p>
-          <p>How can I help you?</p>
-        </div>
-
+         <div className="overflow-hidden">
+      <div
+        className={`bg-gray-800 px-4 py-3 rounded-2xl w-fit max-w-[80%] 
+        transform transition-all duration-1000 ease-out
+        ${show ? "translate-y-0 opacity-100" : "translate-y-20 opacity-0"}`}
+      >
+        <p className="text-sm text-gray-300">AI</p>
+        <p>How can I help you?</p>
+      </div>
+    </div>
         {/* User Message */}
         <div className="bg-blue-600 px-4 py-3 rounded-2xl w-fit max-w-[80%] self-end">
           <p className="text-sm text-blue-200">User</p>
@@ -33,11 +43,11 @@ const Home = () => {
 
       {/* Mic Button */}
       <div className="flex flex-col items-center gap-3">
-        <button className="h-16 w-16 rounded-full bg-blue-600 hover:bg-blue-500 transition flex items-center justify-center text-xl shadow-lg">
+        <button className="h-16 w-16 rounded-full bg-blue-600 hover:bg-blue-500 transition flex items-center justify-center text-xl shadow-lg cursor-pointer hover:scale-105 active:border-3 active:animate-pulse">
           🎤
         </button>
 
-        <p className="text-gray-400 text-sm">Tap to Speak</p>
+        <p className="text-gray-400 text-sm">Hold to Speak</p>
       </div>
 
     </div>
